43f32faca188c1fe94a91ab1b9fbdf068c77968b
JJ <jjgabbott@gmail.com>
2018-05-29 23:46:07 -0400

    fixed a bug when getting exact recipe

--- BOTWCookBook/src/main/java/com/BOTW/CookBook/Recipes/RecipeController.java
+++ BOTWCookBook/src/main/java/com/BOTW/CookBook/Recipes/RecipeController.java
@@ -18,69 +18,69 @@ public class RecipeController {
 	// /GET   ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 	
 	//Gets all recipes
-	@RequestMapping("/find/recipes")
+	@RequestMapping("/api/recipes")
 	public List<Recipe> getRecipes() {
 		return recipeService.getRecipes();
 	}
 	
 	//Gets all recipes by name
-	@RequestMapping("/recipes/{name}")
+	@RequestMapping("/api/recipes/name/{name}")
 	public List<Recipe> getRecipesByName(@PathVariable String name) {
 		return recipeService.getRecipesByName(name);
 	}
 	
 	
 	//Finds the recipe whose ingredients matches exactly the ingredients being searched
-	@RequestMapping("/find/recipes/ingredients/{ingred1}")
+	@RequestMapping("/api/find/recipes/ingredients/{ingred1}")
 	public List<Recipe> getRecipesByIngredients(@PathVariable String ingred1){
-		return recipeService.searchRecipesByIngredients(ingred1);
+		return recipeService.getRecipesByIngredients(ingred1);
 	}
 	
-	@RequestMapping("/find/recipes/ingredients/{ingred1}/{ingred2}")
+	@RequestMapping("/api/find/recipes/ingredients/{ingred1}/{ingred2}")
 	public List<Recipe> getRecipesByIngredients(@PathVariable String ingred1, @PathVariable String ingred2){
 		return recipeService.getRecipesByIngredients(ingred1, ingred2);
 	}
 	
-	@RequestMapping("/find/recipes/ingredients/{ingred1}/{ingred2}/{ingred3}")
+	@RequestMapping("/api/find/recipes/ingredients/{ingred1}/{ingred2}/{ingred3}")
 	public List<Recipe> getRecipesByIngredients(@PathVariable String ingred1, @PathVariable String ingred2, @PathVariable String ingred3){
 		return recipeService.getRecipesByIngredients(ingred1, ingred2, ingred3);
 	}
 		
-	@RequestMapping("/find/recipes/ingredients/{ingred1}/{ingred2}/{ingred3}/{ingred4}")
+	@RequestMapping("/api/find/recipes/ingredients/{ingred1}/{ingred2}/{ingred3}/{ingred4}")
 	public List<Recipe> getRecipesByIngredients(@PathVariable String ingred1, @PathVariable String ingred2, @PathVariable String ingred3, @PathVariable String ingred4){
 		return recipeService.getRecipesByIngredients(ingred1, ingred2, ingred3, ingred4);
 	}
 	
-	@RequestMapping("/find/recipes/ingredients/{ingred1}/{ingred2}/{ingred3}/{ingred4}/{ingred5}")
+	@RequestMapping("/api/find/recipes/ingredients/{ingred1}/{ingred2}/{ingred3}/{ingred4}/{ingred5}")
 	public List<Recipe> getRecipesByIngredients(@PathVariable String ingred1, @PathVariable String ingred2, @PathVariable String ingred3, @PathVariable String ingred4, @PathVariable String ingred5){
 		return recipeService.getRecipesByIngredients(ingred1, ingred2, ingred3, ingred4, ingred5);
 	}
 	
 	
 	//Does a search on recipes that contain at least the ingredients being searched
-	@RequestMapping("/search/recipes/ingredients/{ingred1}")
+	@RequestMapping("/api/search/recipes/ingredients/{ingred1}")
 	public List<Recipe> searchRecipesByIngredients(@PathVariable String ingred1){
 		return recipeService.searchRecipesByIngredients(ingred1);
 	}
 	
-	@RequestMapping("/search/recipes/ingredients/{ingred1}/{ingred2}")
+	@RequestMapping("/api/search/recipes/ingredients/{ingred1}/{ingred2}")
 	public List<Recipe> searchRecipesByIngredients(@PathVariable String ingred1, @PathVariable String ingred2){
 		return recipeService.searchRecipesByIngredients(ingred1, ingred2);
 	}
 	
-	@RequestMapping("/search/recipes/ingredients/{ingred1}/{ingred2}/{ingred3}")
+	@RequestMapping("/api/search/recipes/ingredients/{ingred1}/{ingred2}/{ingred3}")
 	public List<Recipe> searchRecipesByIngredients(@PathVariable String ingred1, @PathVariable String ingred2, @PathVariable String ingred3){
 		return recipeService.searchRecipesByIngredients(ingred1, ingred2, ingred3);
 	}
 		
-	@RequestMapping("/search/recipes/ingredients/{ingred1}/{ingred2}/{ingred3}/{ingred4}")
+	@RequestMapping("/api/search/recipes/ingredients/{ingred1}/{ingred2}/{ingred3}/{ingred4}")
 	public List<Recipe> searchRecipesByIngredients(@PathVariable String ingred1, @PathVariable String ingred2, @PathVariable String ingred3, @PathVariable String ingred4){
 		return recipeService.searchRecipesByIngredients(ingred1, ingred2, ingred3, ingred4);
 	}
 	
 	
 	// /POST  --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-	@RequestMapping(method=RequestMethod.POST, value="/add/recipe")
+	@RequestMapping(method=RequestMethod.POST, value="/api/add/recipe")
 	public boolean addRecipe(@RequestBody Recipe recipe) {
 		return recipeService.addRecipe(recipe);
 	}
